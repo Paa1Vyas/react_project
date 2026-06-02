9f071dfe9bc6e3b7db8197802e68cc538d970819
Paavan Vyas <paavanvyas57@gmail.com>
2026-06-02 11:48:29 +0530

    Displaying Backend Message

--- src/FullStackDemo.jsx
+++ src/FullStackDemo.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import axios from 'axios';
+import cors from 'cors';
 
 export default function FullStackDemo() {
     const url = 'https://k-eep9.onrender.com/hello?name=paavan';
@@ -12,6 +13,8 @@ export default function FullStackDemo() {
     });
   return (
     <div>
+      <h1>Full Stack Demo</h1>
+      <p>{response.data.message}</p>
 
     </div>
   )
